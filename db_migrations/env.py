74250acd11569da6b35a3f386717ea391efd1315
Milan Panic <pax7748@gmail.com>
2026-06-02 22:12:49 +0300

    fixing alembic env variable left after refactor

--- db_migrations/env.py
+++ db_migrations/env.py
@@ -11,7 +11,7 @@ from src.config.settings import settings
 # access to the values within the .ini file in use.
 config = context.config
 
-config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
+config.set_main_option("sqlalchemy.url", f"postgresql://{settings.database_url}")
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
